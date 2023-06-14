fa0a360b07899ac40d6118d6afd752b5710b3627
hieu <hieuvnautotech@gmail.com>
2023-06-14 21:00:30 +0700

    ok

--- budget/src/App.js
+++ budget/src/App.js
@@ -7,7 +7,11 @@ import NewEntryForm from './components/NewEntryForm'
 import DisplayBalance from './components/DisplayBalance'
 import DisplayBalances from "./components/DisplayBalances";
 import EntryLine from "./components/EntryLine";
+import {useState} from 'react'
+
 function App() {
+  const [entries, setEntries] = useState(initialEntries)
+
   return (
     <Container>
       
@@ -25,7 +29,7 @@ function App() {
 
       
 
-      <EntryLine description="Something" value="$10" />
+      <EntryLine description={entries[0].description} value={entries[0].value} isExpense={entries[0].isExpense} />
       <EntryLine description="Expense" value="$10" isExpense />
      
       <MainHeader title="Add new transaction" type="h3" />
@@ -35,3 +39,26 @@ function App() {
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    description : 'work income',
+    value : '$1500',
+    isExpense: false
+  },
+  {
+    description : 'water bill',
+    value : '$100',
+    isExpense: true
+  },
+  {
+    description : 'Rent',
+    value : '$500',
+    isExpense: true
+  },
+  {
+    description : 'Powre bill',
+    value : '$50',
+    isExpense: true
+  }
+]
