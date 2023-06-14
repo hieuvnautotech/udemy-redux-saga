49e4f32ca5733860718e3b2befa28b188bdff7b4
hieu <hieuvnautotech@gmail.com>
2023-06-14 12:46:37 +0700

    ok

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-
+import { Container, Header, Statistic, Segment, Grid } from 'semantic-ui-react'
 function App() {
   return (
-    <div className="App">
-        akjshdkjashdkasd
-    </div>
+    <Container>
+      <Header as="h1">Budget</Header>
+      <Statistic size="small">
+        <Statistic.Label>Your Balance</Statistic.Label>
+        <Statistic.Value>2,55.53</Statistic.Value>
+      </Statistic>
+      <Segment textAlign="center">
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label style={{ texAlign: "left" }}>
+                  Incomming
+                </Statistic.Label>
+                <Statistic.Value>2,55.53</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label style={{ texAlign: "left" }}>
+                  Expenses
+                </Statistic.Label>
+                <Statistic.Value>2,55.53</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Container>
   );
 }
 
