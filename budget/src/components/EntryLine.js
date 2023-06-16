fa13d363b156a3e0e8aae9db334476e5380d0660
hieu <hieuvnautotech@gmail.com>
2023-06-16 14:07:54 +0700

    ok

--- budget/src/components/EntryLine.js
+++ budget/src/components/EntryLine.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { Segment, Grid, Icon} from 'semantic-ui-react'
+import React, { useState} from 'react'
+import { Container, Segment, Grid, Icon } from 'semantic-ui-react'
+import ModalEdit from './ModalEdit'
+
 function EntryLine({ id, description, value, isExpense = false, deleteEntry }) {
-            
+  const [isOpen, setIsOpen] = useState(false)         
              
             
   
   return (
-    <Segment color={isExpense ? "red" : "green"}>
-      <Grid columns={3} textAlign="right">
-        <Grid.Row>
-          <Grid.Column width={10} textAlign="left">
-            {description}
-          </Grid.Column>
-          <Grid.Column width={3} textAlign="right">
-            {value}
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Icon name="edit" bordered />
-            <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+    <Container>
+      <Segment color={isExpense ? "red" : "green"}>
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              {description}
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              {value}
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered onClick={ ()=> setIsOpen(true)} />
+              <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <ModalEdit isOpen={isOpen}/>
+    </Container>
   );
 }
 
