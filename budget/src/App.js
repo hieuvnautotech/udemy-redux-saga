d831575ccee289a904f5ad46992ac8d9b5f5d8ac
hieu <hieuvnautotech@gmail.com>
2023-07-03 22:11:37 +0700

    ok

--- budget/src/App.js
+++ budget/src/App.js
@@ -7,7 +7,7 @@ import DisplayBalances from "./components/DisplayBalances";
 import { useState, useEffect } from "react";
 import EntryLines from "./components/EntryLines";
 import ModalEdit from './components/ModalEdit'
-import {createStore, combineReducers} from 'redux'
+
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [isExpense, setIsExpense] = useState(false);
-  const [entries, setEntries] = useState(initialEntries);
+  // const [entries, setEntries] = useState(initialEntries);
   const [isOpen, setIsOpen] = useState(false)         
   const [entryId, setEntryId] = useState()    
   const [incomeTotal, setIncomeTotal] = useState(0);
@@ -23,119 +23,72 @@ function App() {
   const [total, setTotal] = useState(0)
 
 
-  useEffect(() => {
-    if(!isOpen && entryId){
-      const index = entries.findIndex(entry => entry.id === entryId)
-      const newEntries = [...entries]
-      newEntries[index].description = description
-      newEntries[index].value = value
-      newEntries[index].isExpense = isExpense
-      setEntries(newEntries)
-      resetEntry()
-    }
-  }, [isOpen]);
+  // useEffect(() => {
+  //   if(!isOpen && entryId){
+  //     const index = entries.findIndex(entry => entry.id === entryId)
+  //     const newEntries = [...entries]
+  //     newEntries[index].description = description
+  //     newEntries[index].value = value
+  //     newEntries[index].isExpense = isExpense
+  //     setEntries(newEntries)
+  //     resetEntry()
+  //   }
+  // }, [isOpen]);
 
-  function entriesReducer(state=initialEntries, action) {
-    console.log(action)
-    let newEntries
-    switch (action.type){
-      case 'ADD_ENTRY' : 
-      newEntries = state.concat({...action.payload})
-      return newEntries
+  
 
-      case 'REMOVE_ENTRY' :
-      newEntries = state.filter(entry => entry.id !== action.payload.id)
-      return newEntries
+  
 
-      default:
-      return state
-    }
     
-  }
 
-  const combinedReducers = combineReducers({
-    entries: entriesReducer
-  })  
-  const store = createStore(combinedReducers)
-
-    store.subscribe(() => {
-      console.log('store:',store.getState())
-    })
-  
-  // console.log(`store before`, store.getState())
-
-  const payload_add = {
-    id: 5,
-    description: "hello redux",
-    value: 100,
-    isExpense: false,
-  };
-
-  const payload_remove={
-    id:1,
-  }
-
-  function addEntryRedux(payload){
-    return {type:'ADD_ENTRY', payload}
-  }
-
-  function removeEntryRedux(id){
-    return {type: 'REMOVE_ENTRY', payload: {id}}
-  }
-
-  store.dispatch(addEntryRedux(payload_add))
-  store.dispatch(removeEntryRedux(1))
-  
-  // console.log(`store after`, store.getState())
-
-  useEffect(() => {
-    let totalIncome = 0
-    let totalExpenses = 0
-    entries.map(entry => { 
-      if (entry.isExpense) {
-        return (totalExpenses += Number(entry.value))
-      } else { 
-        return (totalIncome += Number(entry.value))
-      }
-    })
-    setTotal (totalIncome - totalExpenses)
-    setExpenseTotal(totalExpenses)
-    setIncomeTotal(totalIncome)
-  },[entries])
+  // useEffect(() => {
+  //   let totalIncome = 0
+  //   let totalExpenses = 0
+  //   entries.map(entry => { 
+  //     if (entry.isExpense) {
+  //       return (totalExpenses += Number(entry.value))
+  //     } else { 
+  //       return (totalIncome += Number(entry.value))
+  //     }
+  //   })
+  //   setTotal (totalIncome - totalExpenses)
+  //   setExpenseTotal(totalExpenses)
+  //   setIncomeTotal(totalIncome)
+  // },[entries])
 
 
-  function deleteEntry(id) { 
-    const result = entries.filter(entry => entry.id !== id)
-    setEntries(result);
-  }
+  // function deleteEntry(id) { 
+  //   const result = entries.filter(entry => entry.id !== id)
+  //   setEntries(result);
+  // }
 
-  function addEntry() {
-    const result = entries.concat({
-      id: entries.length + 1,
-      description,
-      value,
-      isExpense,
-    });
-    console.log(`result`, result);
-    console.log(`entries`, entries);
-    setEntries(result);
-    resetEntry()
-  }
+  // function addEntry() {
+  //   const result = entries.concat({
+  //     id: entries.length + 1,
+  //     description,
+  //     value,
+  //     isExpense,
+  //   });
+  //   console.log(`result`, result);
+  //   console.log(`entries`, entries);
+  //   setEntries(result);
+  //   resetEntry()
+  // }
 
-  function ediEntry(id){
-    console.log(`edit entry ${id}`)
-    if(id){
-      const index = entries.findIndex(entry => entry.id === id)
-      const entry = entries[index];
-      setEntryId(id)
-      setDescription(entry.description)
-      setValue(entry.value)
-      setIsExpense(entry.isExpense)
-      setIsOpen(true)
+  // function ediEntry(id){
+  //   console.log(`edit entry ${id}`)
+  //   if(id){
+  //     const index = entries.findIndex(entry => entry.id === id)
+  //     const entry = entries[index];
+  //     setEntryId(id)
+  //     setDescription(entry.description)
+  //     setValue(entry.value)
+  //     setIsExpense(entry.isExpense)
+  //     setIsOpen(true)
        
       
-    }
-  }
+  //   }
+  // }
 
   function resetEntry(){
     setDescription('')
@@ -191,29 +144,4 @@ function App() {
 
 export default App;
 
-var initialEntries = [
-  {
-    id:1,
-    description: "work income",
-    value: "1500",
-    isExpense: false,
-  },
-  {
-    id:2,
-    description: "water bill",
-    value: "100",
-    isExpense: true,
-  },
-  {
-    id:3,
-    description: "Rent",
-    value: "500",
-    isExpense: true,
-  },
-  {
-    id:4,
-    description: "Powre bill",
-    value: "50",
-    isExpense: true,
-  },
-];
+
---
+++ budget/src/reducers/entries.reducers.js
@@ -0,0 +1,44 @@
+const reducer = (state=initialEntries, action) => {
+    console.log(action)
+    let newEntries
+    switch (action.type){
+      case 'ADD_ENTRY' : 
+      newEntries = state.concat({...action.payload})
+      return newEntries
+
+      case 'REMOVE_ENTRY' :
+      newEntries = state.filter(entry => entry.id !== action.payload.id)
+      return newEntries
+
+      default:
+      return state
+    }
+    
+  }
+export default reducer
+  var initialEntries = [
+    {
+      id:1,
+      description: "work income",
+      value: "1500",
+      isExpense: false,
+    },
+    {
+      id:2,
+      description: "water bill",
+      value: "100",
+      isExpense: true,
+    },
+    {
+      id:3,
+      description: "Rent",
+      value: "500",
+      isExpense: true,
+    },
+    {
+      id:4,
+      description: "Powre bill",
+      value: "50",
+      isExpense: true,
+    },
+  ];
