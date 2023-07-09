1c99488c7c537bf4b9fd5cd6bd802bd70eaf752a
hieu <hieuvnautotech@gmail.com>
2023-07-09 20:59:28 +0700

    ok

--- budget/src/components/EntryLine.js
+++ budget/src/components/EntryLine.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react'
 import { Container, Segment, Grid, Icon } from 'semantic-ui-react'
 import { useDispatch} from 'react-redux'
 import { removeEntryRedux} from '../actions/entries.actions'
+import {openEditModal} from '../actions/modals.actions'
 
 function EntryLine({ id, description, value, isExpense = false, ediEntry }) {
              
@@ -19,7 +20,7 @@ function EntryLine({ id, description, value, isExpense = false, ediEntry }) {
               {value}
             </Grid.Column>
             <Grid.Column width={3}>
-              <Icon name="edit" bordered onClick={ ()=> ediEntry(id)} />
+              <Icon name="edit" bordered onClick={ ()=> dispatch(openEditModal(id))} />
               <Icon name="trash" bordered onClick={() => dispatch(removeEntryRedux(id))} />
             </Grid.Column>
           </Grid.Row>
