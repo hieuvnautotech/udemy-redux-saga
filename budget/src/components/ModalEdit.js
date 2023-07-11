6c728149ed3df794dab1b0fee348669a529cab35
hieu <hieuvnautotech@gmail.com>
2023-07-11 21:48:34 +0700

    ok

--- budget/src/components/ModalEdit.js
+++ budget/src/components/ModalEdit.js
@@ -3,6 +3,7 @@ import { Modal, Button } from 'semantic-ui-react'
 import EntryForm from './EntryForm'
 import { useDispatch} from 'react-redux'
 import {closeEditModal} from '../actions/modals.actions'
+import useEntryDetails from './../hooks/useEntryDetails';
 
 function ModalEdit({ 
   isOpen, 
@@ -16,18 +17,19 @@ function ModalEdit({
   }) {
   
   const dispatch = useDispatch()
+  const entryUpdate = useEntryDetails(description, value, isExpense)
   return (
       <Modal open={ isOpen}>
           <Modal.Header>Edit entry</Modal.Header>
           <Modal.Content>
               {/* <Modal.Description>asdasdas</Modal.Description> */}
               <EntryForm
-                description={description}
-                setDescription={setDescription}
-                value={value}
-                setValue={setValue}
-                isExpense={isExpense}
-                setIsExpense={setIsExpense}
+                description={entryUpdate.description}
+                setDescription={entryUpdate.setDescription}
+                value={entryUpdate.value}
+                setValue={entryUpdate.setValue}
+                isExpense={entryUpdate.isExpense}
+                setIsExpense={entryUpdate.setIsExpense}
               />
           </Modal.Content>
           <Modal.Actions>
