a3cd924b67b97d0bf52ee8b8fde8dbfbfc338922
hieu <hieuvnautotech@gmail.com>
2023-08-03 21:50:45 +0700

    ok

--- budget/src/store/configureStore.js
+++ budget/src/store/configureStore.js
@@ -3,18 +3,20 @@ import entriesReducer from '../reducers/entries.reducers'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import modalsReducer from '../reducers/modals.reducers'
 import createSagaMiddleware from 'redux-saga'
+import {testSaga} from '../sagas/testSaga'
 
 const sagaMiddleware = createSagaMiddleware()
 const middlewares = [sagaMiddleware]
 
 const configureStore = () => {
-    return createStore(
+    const store = createStore(
         combineReducers({
             entries: entriesReducer,
             modals: modalsReducer,
         }),
         composeWithDevTools((applyMiddleware(...middlewares)))
     )
-    
+    sagaMiddleware.run(testSaga)
+    return store
 }
 export default configureStore
