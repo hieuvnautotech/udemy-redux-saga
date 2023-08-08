da0e8a70ded68ce0ce38b3120261defd2c8af55e
hieuvnautotech <hieuvnautotech@gmail.com>
2023-08-08 19:11:49 +0700

    ok

--- budget/src/components/EntryLine.js
+++ budget/src/components/EntryLine.js
@@ -8,6 +8,7 @@ function EntryLine({ id, description, value, isExpense = false}) {
              
             
   const dispatch = useDispatch()
+  dispatch({type: 'TEST_MESSAGE'})
   return (
     <Container>
       <Segment color={isExpense ? "red" : "green"}>
