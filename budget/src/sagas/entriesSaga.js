9d1608b2ee3224dd6ceac0b57ad6d54c579bc4c2
hieu <hieuvnautotech@gmail.com>
2023-08-15 22:00:11 +0700

    ok

--- budget/src/sagas/entriesSaga.js
+++ budget/src/sagas/entriesSaga.js
@@ -1,6 +1,9 @@
-import {take} from 'redux-saga/effects'
+import {take, call} from 'redux-saga/effects'
 import entriesTypes from '../actions/entries.actions'
+import axios from 'axios'
 export function* getAllEntries(){
     yield take(entriesTypes.GET_ENTRIES)
     console.log('entries saga')
+    const result = yield call (axios, 'http://localhost:3002/entries')
+    console.log(result)
 }
