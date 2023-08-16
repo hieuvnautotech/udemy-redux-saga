21cb0f1589035387530ed9ead2fad8d67ad836f2
hieuvnautotech <hieuvnautotech@gmail.com>
2023-08-16 15:08:40 +0700

    ok

--- budget/src/sagas/entriesSaga.js
+++ budget/src/sagas/entriesSaga.js
@@ -1,4 +1,4 @@
-import {take, call, put} from 'redux-saga/effects'
+import {take, call, put, fork} from 'redux-saga/effects'
 import entriesTypes from '../actions/entries.actions'
 import axios from 'axios'
 import types from '../actions/entries.actions'
@@ -8,4 +8,19 @@ export function* getAllEntries(){
     const result = yield call (axios, 'http://localhost:3002/entries')
     console.log(result)
     yield put({type: types.POPULATE_ENTRIES, payload: result.data})
+}
+
+export function* getEntryDetails(id){
+    // console.log(`got the id ${id}`)
+    const {data} = yield call (axios, `http://localhost:3002/values/${id}`)
+    console.log(data)
+}
+
+export function* getAllEntriesDetails(){
+    const {payload} = yield take(entriesTypes.POPULATE_ENTRIES)
+    for(let index = 0; index<payload.length; index++){
+        const entry = payload[index]
+        yield fork(getEntryDetails, entry.id)
+    }
+   
 }
