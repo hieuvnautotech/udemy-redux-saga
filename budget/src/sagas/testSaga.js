87a41a0ae811201c72c28bb0f3cfdd92af570768
hieu <hieuvnautotech@gmail.com>
2023-08-20 21:13:56 +0700

    ok

--- budget/src/sagas/testSaga.js
+++ budget/src/sagas/testSaga.js
@@ -1,11 +1,11 @@
-import {take, put, delay, call, fork} from 'redux-saga/effects'
+import {take, put, delay, call, fork, takeEvery} from 'redux-saga/effects'
 function double(number){
     return number*2
 }
 
 export function* testSaga(){
     while(1){
-        console.log("Starting saga")
+        console.log('Starting saga')
         const state = yield take('TEST_MESSAGE')
         const a = yield call (double,2)
         console.log(a)
@@ -32,11 +32,27 @@ export function* testSagaFork(){
     }
 }
 
+export function* testSagaTakeEveryProcess({payload}){
+    console.log(`Starting process for index ${payload}`)
+    yield delay(3000)
+    console.log(`Ending process for index ${payload}`)
+
+
+}
+
+
+export function* testSagaTakeEvery(){
+    const{payload} = yield takeEvery('TEST_MESSAGE3', testSagaTakeEveryProcess)
+    console.log(`finish take every index ${payload}`)
+}
+
 
 export function* dispatchTest(){
+    let index = 0
     while (1) {
-       yield delay(5000)
-       yield put({type: 'TEST_MESSAGE2', payload:1000})
+       yield delay(500)
+       yield put({type: 'TEST_MESSAGE3', payload: index})
+       index++
     }
 }
 
