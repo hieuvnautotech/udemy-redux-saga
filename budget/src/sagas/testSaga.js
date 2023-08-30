402eedc89e0eac0b20ac7ad81dbbbc0b718b37eb
hieuvnautotech <hieuvnautotech@gmail.com>
2023-08-30 15:41:49 +0700

    ok

--- budget/src/sagas/testSaga.js
+++ budget/src/sagas/testSaga.js
@@ -5,8 +5,8 @@ function double(number){
 
 export function* testSaga(){
     while(1){
-        console.log('Starting saga')
-        const state = yield take('TEST_MESSAGE')
+        console.log('Starting saga') // đầu tiên vô chạy cái này do được gọi bên index saga
+        const state = yield take('TEST_MESSAGE') // chạy tới đây thì dừng đi tới dispatchTest chạy put
         const a = yield call (double,2)
         console.log(a)
         const b = yield double(3)
@@ -15,6 +15,30 @@ export function* testSaga(){
     }
     
 }
+
+export function* dispatchTest(){
+    let index = 0
+    while (1) {
+       yield delay(500)
+       yield put({type: 'TEST_MESSAGE3', payload: index}) // TEST_MESSAGE3 được gọi thì index + lên
+       index++
+    }
+}
+
+export function* testSagaTakeEvery(){
+    const{payload} = yield takeEvery('TEST_MESSAGE3', testSagaTakeEveryProcess) // takeEvery này ko có blocking, lắng nghe TEST_MESSAGE3 và gọi testSagaTakeEveryProcess rồi đi
+    console.log(`finish take every index ${payload}`) // show cái log finish này ra
+}
+
+export function* testSagaTakeEveryProcess({payload}){
+    console.log(`Starting process for index ${payload}`)
+    yield delay(3000)
+    console.log(`Ending process for index ${payload}`)
+
+
+}
+
+
 
 function* doNothing(){
     console.log('have been called')
@@ -32,27 +56,11 @@ export function* testSagaFork(){
     }
 }
 
-export function* testSagaTakeEveryProcess({payload}){
-    console.log(`Starting process for index ${payload}`)
-    yield delay(3000)
-    console.log(`Ending process for index ${payload}`)
 
 
-}
 
 
-export function* testSagaTakeEvery(){
-    const{payload} = yield takeEvery('TEST_MESSAGE3', testSagaTakeEveryProcess)
-    console.log(`finish take every index ${payload}`)
-}
 
 
-export function* dispatchTest(){
-    let index = 0
-    while (1) {
-       yield delay(500)
-       yield put({type: 'TEST_MESSAGE3', payload: index})
-       index++
-    }
-}
+
 
