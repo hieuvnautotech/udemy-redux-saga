eb5941f3dbe5e634bc0755b60164ce7ada3815fc
hieu <hieuvnautotech@gmail.com>
2023-09-04 10:58:25 +0700

    ok

--- budget/src/sagas/entriesSagaAdd.js
+++ budget/src/sagas/entriesSagaAdd.js
@@ -1,7 +1,32 @@
 import axios from 'axios'
-import {take, call, put} from 'redux-saga/effects';
+import {take, call, put, takeLatest} from 'redux-saga/effects';
 import entriesTypes from '../actions/entries.actions'
-export function* deleteEntrySaga(){
-    
-}
+
+export function* addEntrySaga() {
+    yield takeLatest(entriesTypes.ADD_ENTRY, addEntryToDb);
+  }
+
+function* addEntryToDb({payload}) {
+    console.log('addEntry', payload)
+    yield call(addEntry, payload);
+    yield call(addEntryDetails, payload);
+    // yield put({ type: entriesTypes.ADD_ENTRY_RESULT, payload });
+  }
+
+  async function addEntry({ id, description }) {
+    // console.log('entry',id, description)
+    await axios.post('http://localhost:3002/entries', {
+      id,
+      description,
+    });
+  }
+
+  async function addEntryDetails({ id, isExpense, value }) {
+    // console.log('detail',id, isExpense, value)
+    await axios.post('http://localhost:3002/values', {
+      id,
+      isExpense,
+      value,
+    });
+  }
 
